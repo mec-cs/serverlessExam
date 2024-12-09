9b601fcfa3b81081e726fc1185843220a11da043
Enes <102901204+mec-cs@users.noreply.github.com>
2024-12-09 15:09:06 +0300

    Function to search for matching transporters

--- question1/answer.ts
+++ question1/answer.ts
@@ -1,1 +1,69 @@
-console.log('Hello world')
+interface Car {
+    make: string;
+    color: string;
+    registration: string;
+    owner: string;
+}
+
+interface Bicycle {
+    make: string;
+    color: string;
+    owner: string;
+}
+
+const carpark: Car[] = [
+    {
+        make: "Toyota Yaris",
+        color: "Red",
+        registration: "231WD1234",
+        owner: "Jane Smith",
+    },
+    {
+        make: "Suzuki Swift",
+        color: "Blue",
+        registration: "241WD4321",
+        owner: "Paul O Regan",
+    },
+    {
+        make: "Ford Puma",
+        color: "Blue",
+        registration: "241WD1212",
+        owner: "Eileen Silk",
+    },
+];
+
+const bicycleShed: Bicycle[] = [
+    {
+        make: "Revel Rascal XO",
+        color: "Blue",
+        owner: "Cindy Tamoka",
+    },
+    {
+        make: "Yeti SB140 LR",
+        color: "Red",
+        owner: " ",
+    },
+];
+
+function getCarMatches(data: Car[], criteria: (t: Car) => boolean): Car[] {
+    return data.filter(criteria);
+}
+
+function getBicycleMatches(
+    data: Bicycle[],
+    criteria: (t: Bicycle) => boolean
+): Bicycle[] {
+    return data.filter(criteria);
+}
+
+// Generic function to return matched data
+function getMatches<T>(data: T[], criteria: (d: T) => boolean): T[] | undefined {
+    const matches = data.filter(criteria);
+    return matches.map((f) => f);
+}
+
+// It will return Blue Bicycles
+console.log(getMatches<Bicycle>(bicycleShed, (b) => b.color == "Blue"))
+
+// It will return Red Cars
+console.log(getMatches<Car>(carpark, (b) => b.color == "Red"))
